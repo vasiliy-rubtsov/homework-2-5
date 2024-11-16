d37c6f3e3d6369417713ba5bbf10a780574c665e
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2024-11-16 21:06:21 +0300

    Домашнее задание 2.5 по теме "Исключения"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String login = "vasiliy_rubtsov";
+        String password = "390Ax.BWlb";
+        String confirmPassword = "390Ax_BWlb";
+
+        Validator.validate(login, password, confirmPassword);
+
+        System.out.println("Логин: " + login);
+        System.out.println("Пароль: " + password);
+        System.out.println("Подтверждение пароля: " + confirmPassword);
+
     }
+
 }
